836599d7557044e1dbc803adb8295ef9aa5b9ef5
mmjobst <62211266+mmjobst@users.noreply.github.com>
2023-06-26 18:17:43 +0200

    added new kata

---
+++ src/main/java/org/example/collatz/Collatz.java
@@ -0,0 +1,24 @@
+package org.example.collatz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collatz {
+
+    public String generateCollatz(int n) {
+        List<String> sequence = new ArrayList<>();
+        sequence.add(Integer.toString(n));
+
+        if (n == 0) {
+            sequence.add("1");
+        }
+
+        while (n > 1) {
+            n = (n % 2 == 0) ? (n / 2) : (n * 3 + 1);
+            sequence.add(Integer.toString(n));
+        }
+
+        return String.join("->", sequence);
+    }
+
+}
